35ad587604e20924bb236e5a0866a57ac9f0ae55
RaceWilliams3 <willr108@my.erau.edu>
2022-09-03 19:08:36 -0700

    Wall Running Camera Tilt

--- RW_M01_PR01_CubethonPlus/Assets/Scripts/FollowPlayer.cs
+++ RW_M01_PR01_CubethonPlus/Assets/Scripts/FollowPlayer.cs
@@ -13,11 +13,12 @@ public class FollowPlayer : MonoBehaviour
 
     private Vector3 targetRotation;
     private Vector3 defaultRotation = new Vector3(0f, 0f, 0f);
+
+    private Vector3 rotationFromWall;
+
     private float tiltPortion;
-    private float tiltTime = 0.05f;
+    private float tiltTime = 0.2f;
     private float elapsedTime = 0f;
-
-
 
 
     // Update is called once per frame
@@ -28,29 +29,45 @@ public class FollowPlayer : MonoBehaviour
         collision = player.GetComponent<PlayerCollision>();
 
         //Update position based on player position and offset
-        transform.position =  playerPosition.position + offset;
+        transform.position = playerPosition.position + offset;
 
-        
+
     }
 
     void FixedUpdate()
     {
-        
+
         collision = player.GetComponent<PlayerCollision>();
+
+        
 
         if (collision.collisionDirection != 0)
         {
-            Debug.Log(elapsedTime);
-            elapsedTime += Time.deltaTime;
-            tiltPortion = elapsedTime / tiltTime;
             targetRotation = new Vector3(0f, 0f, collision.collisionDirection * wallTilt);
+            if (elapsedTime > tiltTime)
+            {
+                elapsedTime = tiltTime;
+            }
         }
         else
         {
+            if (elapsedTime == tiltTime)
+            {
+                elapsedTime = 0;
+            }
+            targetRotation = new Vector3(0f, 0f, 0f);
+        }
+        if (elapsedTime > tiltTime)
+        {
             elapsedTime = 0;
-            targetRotation = defaultRotation;
+        }
+        else
+        {
+            elapsedTime += Time.deltaTime;
         }
 
+        tiltPortion = elapsedTime / tiltTime;
         transform.localEulerAngles = Vector3.Lerp(defaultRotation, targetRotation, tiltPortion);
+
     }
 }
